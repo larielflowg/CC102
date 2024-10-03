83ddd06ac86468227a3e6bc90797f7403c4051a3
larielflowg <larieldconmigo46@gmail.com>
2024-10-03 15:32:17 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using namespace std;
+
+int main ()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer studies" << endl;
+	cout << "BSIT-1B" << endl;
+	cout << "Lariel D. Conmigo" << endl;
+	cout << "Moreno, Daet, Camarines Norte" << endl;
+	return 0;
+}

7b3c64242b65238915f456f912428ccba0f3dc36
larielflowg <larieldconmigo46@gmail.com>
2024-10-03 15:34:41 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+using namespace std;
+
+int main ()
+{
+	string nickname, course, school;
+	cout << "Nickname: ";
+	cin >> nickname;
+	cout << "Course-Year level:";
+	cin >> course;
+	cout << "school:";
+	cin >> school;
+	
+	return 0;
+}
